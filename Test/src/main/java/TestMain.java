10f575df3bfe20788968b0b8138ccb12a7b2ecc4
liyl <liyl@hsit.com.cn>
2022-11-11 14:01:51 +0800

    输出changeResult方法结果

--- Test/src/main/java/TestMain.java
+++ Test/src/main/java/TestMain.java
@@ -6,6 +6,8 @@
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("这是一句话" + "，现在这句话被修改了");
+        TestMain testMain = new TestMain();
+        System.out.println(testMain.changeResult());
 
     }
 
