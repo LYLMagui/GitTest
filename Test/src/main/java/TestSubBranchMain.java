21d4b25a1b3c80b4221c061bcc2e894a913996de
liyl <liyl@hsit.com.cn>
2022-11-11 13:46:41 +0800

    新增TestSubBranchMain类

---
+++ Test/src/main/java/TestSubBranchMain.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: lyl
+ * @CreateTime: 2022/11/11 13:45
+ * @Description:
+ **/
+public class TestSubBranchMain {
+    public static void main(String[] args) {
+        System.out.println("这是Test_branch的子分支");
+    }
+}
