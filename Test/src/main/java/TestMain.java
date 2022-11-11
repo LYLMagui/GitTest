5fb3007ead7431c39a14f3cce75cf8a9d3fa79b2
liyl <liyl@hsit.com.cn>
2022-11-11 14:19:34 +0800

    添加changeResult方法，改变main方法输出结果

--- Test/src/main/java/TestMain.java
+++ Test/src/main/java/TestMain.java
@@ -5,6 +5,14 @@
  **/
 public class TestMain {
     public static void main(String[] args) {
-        System.out.println("这是一句话");
+        System.out.println("这是一句话" + "，现在这句话被修改了");
+        TestMain testMain = new TestMain();
+        System.out.println(testMain.changeResult());
+
+    }
+
+    public String changeResult(){
+        String word = "现在又添加了一句话";
+        return word;
     }
 }
