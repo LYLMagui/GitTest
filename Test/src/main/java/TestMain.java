5553b8446419813ac0766ee966ae62d66ebdf128
liyl <liyl@hsit.com.cn>
2022-11-15 09:43:52 +0800

    Revert "回滚到新增addOneWord方法"
    This reverts commit 18590eb9

--- Test/src/main/java/TestMain.java
+++ Test/src/main/java/TestMain.java
@@ -6,17 +6,11 @@
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("这是一句话" + "，现在这句话被修改了");
-        TestMain testMain = new TestMain();
-        System.out.println(testMain.addOneWord());
+
     }
 
     public String changeResult(){
         String word = "现在又添加了一句话";
-        return word;
-    }
-
-    public String addOneWord(){
-        String word = "git提交给我搞麻了";
         return word;
     }
 }
