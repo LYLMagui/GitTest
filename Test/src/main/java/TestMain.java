5a000ebccc332c9fef9c2fc1f5f1aa83036fb6ec
liyl <liyl@hsit.com.cn>
2022-11-15 09:47:28 +0800

    Revert "回滚到“TestMain新增addOneWord方法”"
    This reverts commit 64ce4649

---
+++ Test/src/main/java/TestMain.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: lyl
+ * @CreateTime: 2022/11/11 13:36
+ * @Description:
+ **/
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("这是一句话" + "，现在这句话被修改了");
+
+    }
+}
